86a1ded837a16c12f4789b13a0ebf5e5e15b2adb
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-10-20 11:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const button=document.getElementById("enterBtn");
+const para=document.getElementById("status");
+const newpara= document.createElement("h1");
+newpara.innerText="Entered Metaverse";
+function changeP(){
+	
+	document.body.insertBefore(newpara, para);
+	para.remove();
+}
+button.addEventListener("click", changeP);
